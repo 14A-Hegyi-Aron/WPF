625ac069aca27b78c7bf0fd6911de0f8fdcf002f
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2022-11-08 11:32:38 +0100

    2022.11.08

--- TravelAgency/TravelAgencyWPF/Pages/OffersPage.xaml.cs
+++ TravelAgency/TravelAgencyWPF/Pages/OffersPage.xaml.cs
@@ -24,5 +24,10 @@ namespace TravelAgencyWPF.pages
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
