22b14637f0aff777d1a0e0d16a7f38c5c680d5f5
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2022-11-10 13:27:03 +0100

    2022.11.10

--- TravelAgency/TravelAgencyWPF/Pages/OffersPage.xaml.cs
+++ TravelAgency/TravelAgencyWPF/Pages/OffersPage.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TravelAgency.Data.Models;
+using TravelAgency.Data.Repositories;
 
 namespace TravelAgencyWPF.pages
 {
@@ -20,14 +22,53 @@ namespace TravelAgencyWPF.pages
     /// </summary>
     public partial class OffersPage : Page
     {
+        private OfferSearchModel searchModel = new();
         public OffersPage()
         {
             InitializeComponent();
         }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            TravelModeRepository travelModeRepository = new();
+            cboTravelMode.ItemsSource = travelModeRepository.GetAll();
+            cboTravelMode.DisplayMemberPath = "Name";
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+            grpSearch
+        }
+
+        private void New_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void Price_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (!decimal.TryParse(textBox.Text, out decimal x))
+            {
+                textBox.Text = null;
+            }
         }
     }
 }
